7374b9760554ec091158f9d4d0d438c8786f8493
Rahul R P <rahulrp.india@gmail.com>
2016-05-29 15:41:03 +0530

    first commit - template setup

---
+++ application/views/user/template/footer.php
@@ -0,0 +1,76 @@
+            <!-- Footer Start -->
+            <footer class="footer">
+                2015 © Velonic.
+            </footer>
+            <!-- Footer Ends -->
+
+
+
+        </section>
+        <!-- Main Content Ends -->
+        
+
+
+        <!-- js placed at the end of the document so the pages load faster -->
+        <script src="<?php echo base_url('assets_user/js/jquery.js'); ?>"></script>
+        <script src="<?php echo base_url('assets_user/js/bootstrap.min.js'); ?>"></script>
+        <script src="<?php echo base_url('assets_user/js/modernizr.min.js'); ?>"></script>
+        <script src="<?php echo base_url('assets_user/js/pace.min.js'); ?>"></script>
+        <script src="<?php echo base_url('assets_user/js/wow.min.js'); ?>"></script>
+        <script src="<?php echo base_url('assets_user/js/jquery.scrollTo.min.js'); ?>"></script>
+        <script src="<?php echo base_url('assets_user/js/jquery.nicescroll.js'); ?>" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets_user/assets/chat/moment-2.2.1.js'); ?>"></script>
+
+        <!-- Counter-up -->
+        <script src="<?php echo base_url('assets_user/js/waypoints.min.js'); ?>" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets_user/js/jquery.counterup.min.js'); ?>" type="text/javascript"></script>
+
+        <!-- EASY PIE CHART JS -->
+        <script src="<?php echo base_url('assets_user/assets/easypie-chart/easypiechart.min.js'); ?>"></script>
+        <script src="<?php echo base_url('assets_user/assets/easypie-chart/jquery.easypiechart.min.js'); ?>"></script>
+        <script src="<?php echo base_url('assets_user/assets/easypie-chart/example.js'); ?>"></script>
+
+
+        <!--C3 Chart-->
+        <script src="<?php echo base_url('assets_user/assets/c3-chart/d3.v3.min.js'); ?>"></script>
+        <script src="<?php echo base_url('assets_user/assets/c3-chart/c3.js'); ?>"></script>
+
+        <!--Morris Chart-->
+        <script src="<?php echo base_url('assets_user/assets/morris/morris.min.js'); ?>"></script>
+        <script src="<?php echo base_url('assets_user/assets/morris/raphael.min.js'); ?>"></script>
+
+        <!-- sparkline --> 
+        <script src="<?php echo base_url('assets_user/assets/sparkline-chart/jquery.sparkline.min.js'); ?>" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets_user/assets/sparkline-chart/chart-sparkline.js'); ?>" type="text/javascript"></script> 
+
+        <!-- sweet alerts -->
+        <script src="<?php echo base_url('assets_user/assets/sweet-alert/sweet-alert.min.js'); ?>"></script>
+        <script src="<?php echo base_url('assets_user/assets/sweet-alert/sweet-alert.init.js'); ?>"></script>
+
+        <script src="<?php echo base_url('assets_user/js/jquery.app.js'); ?>"></script>
+        <!-- Chat -->
+        <script src="<?php echo base_url('assets_user/js/jquery.chat.js'); ?>"></script>
+        <!-- Dashboard -->
+        <script src="<?php echo base_url('assets_user/js/jquery.dashboard.js'); ?>"></script>
+
+        <!-- Todo -->
+        <script src="<?php echo base_url('assets_user/js/jquery.todo.js'); ?>"></script>
+
+
+        <script type="text/javascript">
+        /* ==============================================
+             Counter Up
+             =============================================== */
+            jQuery(document).ready(function($) {
+                $('.counter').counterUp({
+                    delay: 100,
+                    time: 1200
+                });
+            });
+        </script>
+    
+
+    </body>
+
+<!-- Mirrored from coderthemes.com/velonic_2.1/admin/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 22 Dec 2015 15:58:41 GMT -->
+</html>
